fb2e47ee7eddc6a44efa13a27fb0486ee8b485aa
yangsfang <31492969+yangsfang@users.noreply.github.com>
2022-10-30 01:24:07 -0400

    Upgrade counter project dependencies and SDK

--- counter/android/app/src/main/kotlin/com/example/counter/MainActivity.kt
+++ counter/android/app/src/main/kotlin/com/example/counter/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.counter
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
